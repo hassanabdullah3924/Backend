9e2dea73bd4797c2bfea146f79d727a7be81c0df
hassanabdullah3924 <hassanabdullah3924@gmail.com>
2019-11-04 20:48:25 +0400

    Back end to Git

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Imports the express package into your file
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const User = require('./models/User');
@@ -10,15 +10,15 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-const db = 'mongodb+srv://hassan3924:Pronational1@cluster0-mzvaf.mongodb.net/test?retryWrites=true&w=majority'
+const db = 'mongodb+srv://hassan3924:Pronational1@cluster0-mzvaf.mongodb.net/test?retryWrites=true&w=majority';
 mongoose
 .connect(db, {useNewUrlParser: true, useUnifiedTopology: true}) //Promise  
 .then(() => {
     console.log('DB is connected');
 })
 .catch((err)=> {
-    console.log('error', err )
-})
+    console.log('error', err );
+});
 
 
 
@@ -34,17 +34,17 @@ app.get('/', (req, res)=> {
     <p>${req.query.year}</p>
 
     <p>${req.query.industry}</p>
-    `)
+    `);
 });
 
 app.get('/about', (req, res)=> {
     
-    res.send("<h1>About Page</h1>")
+    res.send("<h1>About Page</h1>");
    
 });
 
 app.get('/contact', (req, res)=> {
-    res.send("<h1>Contact Page </h1>")
+    res.send("<h1>Contact Page </h1>");
 });
 
 // ":" turns dynamic into a variable
@@ -52,7 +52,7 @@ app.get('/contact', (req, res)=> {
 app.get('/blog/:page', (req, res)=> {
 
     const page = req.params.page;
-    res.send("<h1>Welcome to " + page + "</h1>")
+    res.send("<h1>Welcome to " + page + "</h1>");
 });
 
 app.post('/register',(req, res)=>{
@@ -65,8 +65,8 @@ app.post('/register',(req, res)=>{
          password : req.body.password
 
 
-        }
-        const newUser = new User(formData)
+        };
+        const newUser = new User(formData);
 
 newUser
 .save() // Promise
@@ -74,13 +74,13 @@ newUser
 // If promise if fulfilled
 .then((newUserData) => {
     // Send response in the form of JSON
-    res.json(newUserData)
+    res.json(newUserData);
 
     // Otherwise...
-})  
-.catch((err) => {
-    console.log('error', err)
 })
+.catch((err) => {
+    console.log('error', err);
+});
 });
 
 app.post('/Feed',(req,res)=>{
@@ -92,8 +92,8 @@ app.post('/Feed',(req,res)=>{
         likes: req.body.likes,
         shares: req.body.shares,
         date: req.body.date
-    }
-    const newFeed = new Feed(feedData)
+    };
+    const newFeed = new Feed(feedData);
 
     newFeed
     .save() // Promise
@@ -101,15 +101,15 @@ app.post('/Feed',(req,res)=>{
     // If promise if fulfilled
     .then((newFeedData) => {
         // Send response in the form of JSON
-        res.json(newFeedData)
+        res.json(newFeedData);
     
         // Otherwise...
     })  
     .catch((err) => {
-        console.log('error', err)
-    })
+        console.log('error', err);
+    });
     
-})
+});
 
 
 
@@ -120,9 +120,9 @@ app.post('/Feed',(req,res)=>{
  without it you won't be able to tell if you are connected*/
 
 app.listen(3000, () => {           
-    console.log('You are connected!')
-    console.log("Hooray!")
-})
+    console.log('You are connected!');
+    console.log("Hooray!");
+});
 
 
 
