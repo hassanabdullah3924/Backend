6cbc82b659c2e7c7abcbfbde46b210dc7fc6ea7a
hassanabdullah3924 <hassanabdullah3924@gmail.com>
2019-11-09 14:33:48 +0400

    Bcrypted  code

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 const UserModel = require('../models/UserModel');
 
@@ -13,23 +14,48 @@ router.post('/register', (req, res)=>{
 
     const newUser = new UserModel(formData);
 
-    newUser
-    .save() // Promise
-
-    // If promise is fulfilled
-    .then( 
-        (newUserData)=>{
-            // Send response in the form of JSON
-            res.json(newUserData)
+    // Step 1. Generate a salt (random data for adding complexity)
+    bcrypt.genSalt((err, salt)=>{
+        if(err) {
+            console.log('error is', err);
         }
-    )
+        
+        // Step 2. Generate a hashed password using (a) the user's password
+        // and (b) the salt
+        bcrypt.hash(
+            newUser.password,
+            salt,
+            (err, hashedPassword)=>{
+                if(err) {
+                    console.log('error is', err);
+                }
+                
+                // Step 3. Reassign the user's password to be hashed password
+                newUser.password = hashedPassword;
 
-    // Otherwise...
-    .catch(
-        (err)=>{
-            console.log('error', err);
-        }
-    )
+                // Step 4. Saving the formData to the database
+
+                newUser
+                .save() // Promise
+
+                // If promise is fulfilled
+                .then( 
+                    (newUserData)=>{
+                        // Send response in the form of JSON
+                        res.json(newUserData)
+                    }
+                )
+                // Otherwise...
+                .catch(
+                    (err)=>{
+                        console.log('error', err);
+                    }
+                )
+            }
+        )
+    });
+
+
 });
 
 
